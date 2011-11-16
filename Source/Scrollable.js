54bf3ea81d2288bc9ed9d310350789bd71eee038
Julian Lam <julian.lam@gmail.com>
2011-11-16 10:28:27 -0500

    updated to support some options for fading and auto hiding, also tweaked container width detection for repositioning

--- Source/Scrollable.js
+++ Source/Scrollable.js
@@ -13,11 +13,16 @@ provides: [Scrollable]
 */
 
 var Scrollable = new Class({
-	initialize: function(element) {
+	initialize: function(element, options) {
 		// Renders a scrollbar over the given element
 		this.element = element;
 		this.active = false;
 		var scrollable = this;
+
+		// Some default options
+		options = options || {};
+		options.autoHide = (typeOf(options.autoHide) != 'null' ? options.autoHide : 1);
+		options.fade = (typeOf(options.fade) != 'null' ? options.fade : 1);
 
 		this.container = new Element('div', {
 			'class': 'scrollbar',
@@ -34,7 +39,9 @@ var Scrollable = new Class({
 		// Making the element scrollable via mousewheel
 		element.addEvent('mouseover', function() {
 			if (this.scrollHeight > this.clientHeight) {
-				scrollable.container.fade('in');
+				if (options.autoHide && options.fade) scrollable.container.fade('in');
+				else if (options.autoHide && !options.fade) scrollable.container.fade('show');
+
 				this.addEvent('mousewheel', function(event) {
 					event.preventDefault();	// Stops the entire page from scrolling
 					this.scrollTop = this.scrollTop - (event.wheel * 30);
@@ -43,28 +50,31 @@ var Scrollable = new Class({
 			}
 		});
 		element.addEvent('mouseout', function() {
-			scrollable.container.fade('out');
+			if (options.autoHide && options.fade) scrollable.container.fade('out');
+			else if (options.autoHide && !options.fade) scrollable.container.fade('hide');
+
 			this.removeEvents('mousewheel');
 		});
 		window.addEvent('resize', function() {
 			scrollable.reposition.delay(50,scrollable);
 		});
 
-		// Fading the container after some time
-		scrollable.container.fade('hide');
+		// Initial hiding of the scrollbar
+		if (options.autoHide) scrollable.container.fade('hide');
 
 		return this;
 	},
 	reposition: function() {
 		// Repositions the scrollbar by rereading the container element's dimensions/position
 		var initSize = this.element.getComputedSize();
+		console.log(initSize);
+		var containerSize = this.container.getSize();
+		console.log(containerSize);
 		var initPos = this.element.getPosition();
 		this.container.setStyles({
 			height: initSize['height'],
-			width: '21px',
-			position: 'absolute',
-			top: (initPos.y+initSize['computedTop'])+'px',
-			left: (initPos.x+initSize['totalWidth']-21)+'px'
+			top: (initPos.y+initSize['computedTop']),
+			left: (initPos.x+initSize['totalWidth']-containerSize.x)
 		});
 		this.slider.autosize();
 	}
